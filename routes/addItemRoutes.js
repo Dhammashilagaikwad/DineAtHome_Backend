1b2ccaae45b209bdc8b6ae1b2c69bff250981b40
Dhammashila <dhammashila025@gmail.com>
2024-10-10 01:21:50 +0530

    handing menu images

--- routes/addItemRoutes.js
+++ routes/addItemRoutes.js
@@ -20,15 +20,15 @@ router.delete('/deleteItem/:id', deleteItemById);
 router.post('/:chefId', uploadItemImage.single('foodPhoto'), addItemByChef); // Post item associated with chefId
 router.get('/getitembychefid/:chefId', getItemsByChefId); // GET route for items by chefId
 
-// New route to fetch all items
-router.get('/items', async (req, res) => {
-  try {
-    const items = await Item.find(); // Fetch all items
-    res.status(200).json(items);
-  } catch (error) {
-    res.status(500).json({ message: 'Error fetching items', error });
-  }
-});
+// // New route to fetch all items
+// router.get('/items', async (req, res) => {
+//   try {
+//     const items = await Item.find(); // Fetch all items
+//     res.status(200).json(items);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error fetching items', error });
+//   }
+// });
 
 
 module.exports = router;
